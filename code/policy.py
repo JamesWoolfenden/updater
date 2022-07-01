20370a552a34b2d73609241eb5a6f9fc076089f0
kartikp10 <kpande479@gmail.com>
2022-07-01 08:36:10 -0400

    parse json components policy

--- code/policy.py
+++ code/policy.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from json.decoder import JSONDecodeError
+import logging
 import connect
 import psycopg2
 import boto3
@@ -11,19 +13,26 @@ from io import BytesIO
 
 
 def update(dn, du, dh, dbp, incident_id, REGION, account_id):
-    policy= make(account_id)
-    token = connect.getdbtoken(DBHostname=dh, Port=dbp, DBUsername=du, Region=REGION)
+    policy = make(account_id)
+    if not policy:
+        logging.warn("No updates were made to the database")
+        return
+    logging.info(f"Following policy will be added: {policy}")
+    token = connect.getdbtoken(
+        DBHostname=dh, Port=dbp, DBUsername=du, Region=REGION)
 
     # Connect to an existing database
-    conn=psycopg2.connect(host=dh, port=dbp, database=dn, user=du, password=token, sslrootcert="global-bundle.cer")
-    
+    conn = psycopg2.connect(host=dh, port=dbp, database=dn,
+                            user=du, password=token, sslrootcert="global-bundle.cer")
+
     # Open a cursor to perform database operations
     cur = conn.cursor()
 
-    cur.execute(f"UPDATE {dn}.incident_configurations SET code='{json.dumps(policy)}' WHERE incident_id='{incident_id}';")
-    
+    cur.execute(
+        f"UPDATE {dn}.incident_configurations SET code='{json.dumps(policy)}' WHERE incident_id='{incident_id}';")
+
     print(cur.statusmessage)
-    
+
     # Make the changes to the database persistent
     conn.commit()
 
@@ -31,57 +40,81 @@ def update(dn, du, dh, dbp, incident_id, REGION, account_id):
     cur.close()
     conn.close()
 
+
 def make(account_id):
 
-    filename="dynamictags.yaml"
-    bucket=f"dynamic-policy-{account_id}"
-    components=get_tags(account_id)
-
+    filename = "dynamictags.yaml"
+    bucket = f"dynamic-policy-{account_id}"
+    components = get_tags(account_id)
+    if not components:
+        logging.warn("No components were detected.")
+        return
     file_loader = FileSystemLoader('templates')
-    env = Environment(loader=FileSystemLoader(os.path.abspath(os.path.dirname(__file__))))
-    
+    env = Environment(loader=FileSystemLoader(
+        os.path.abspath(os.path.dirname(__file__))))
     template = env.get_template('/dynamictags.yaml.tmpl')
     
     output = template.render(components=components)
     return output
 
+
 def get_tags(account_id):
-    bucket=f"dynamic-policy-{account_id}"
+    """
+    Tags should be in the following format:
+    { "tags": { "components": ["a", "b"] } }
+    """
+    bucket = f"dynamic-policy-{account_id}"
     session = boto3.Session()
     s3_client = session.client("s3")
-
     f = BytesIO()
     s3_client.download_fileobj(bucket, "components", f)
-    raw=f.getvalue()
-    rawstring=raw.decode("UTF-8")
-    components=rawstring.split("\n")
-    
+    raw = f.getvalue()
+    rawstring = raw.decode("UTF-8")
+    try:
+        tags_json = json.loads(rawstring)
+        tags_json = tags_json.get('tags')
+        if tags_json:
+            if tags_json.get('components'):
+                components = tags_json.get('components')
+                if not isinstance(components, list):
+                    components = None         
+        else:
+            components = None
+    except JSONDecodeError as e:
+        logging.error(f"Error occurred while parsing JSON from S3: {e}")
     return components
-    
+
+
 def exists(dh, dbp, du, dn, REGION, incident_id):
-    token = connect.getdbtoken(DBHostname=dh, Port=dbp, DBUsername=du, Region=REGION)
+    token = connect.getdbtoken(
+        DBHostname=dh, Port=dbp, DBUsername=du, Region=REGION)
 
     try:
-        conn = psycopg2.connect(host=dh, port=dbp, database=dn, user=du, password=token, sslrootcert="global-bundle.cer")
+        conn = psycopg2.connect(host=dh, port=dbp, database=dn,
+                                user=du, password=token, sslrootcert="global-bundle.cer")
         cur = conn.cursor()
-        cur.execute(f"SELECT count(*) from {dn}.incident_configurations where incident_id='{incident_id}';")
-        results = cursor.fetchone()
-  
+        cur.execute(
+            f"SELECT count(*) from {dn}.incident_configurations where incident_id='{incident_id}';")
+        results = cur.fetchone()
+
         print(results)
-        if results==0:
+        if results == 0:
             return False
         return True
     except Exception as e:
         print("Database connection failed due to {}".format(e))
         return False
-        
+
+
 def insert(dh, dbp, du, dn, REGION, incident_id):
-    token = connect.getdbtoken(DBHostname=dh, Port=dbp, DBUsername=du, Region=REGION)
+    token = connect.getdbtoken(
+        DBHostname=dh, Port=dbp, DBUsername=du, Region=REGION)
 
     try:
-        conn = psycopg2.connect(host=dh, port=dbp, database=dn, user=du, password=token, sslrootcert="global-bundle.cer")
+        conn = psycopg2.connect(host=dh, port=dbp, database=dn,
+                                user=du, password=token, sslrootcert="global-bundle.cer")
         cur = conn.cursor()
-        query=f"INSERT INTO {dn}.incident_configurations (incident_id, incident_type, category, title, guideline, severity, lacework_violation_id, prowler_violation_id, checkov_check_id, remediation_ids, condition_query, is_custom, customer_name, resource_types, provider, created_by, code, constructive_title, descriptive_title, pc_policy_id, frameworks, pc_severity, source_incident_id, additional_pc_policy_ids) VALUES ('{incident_id}', 'Violation', 'General', 'Ensure Security group has correct tags', 'url to guideline', 'MEDIUM', NULL, NULL, '{incident_id}','{{}}', NULL, true, 'johndeere', '{{aws_security_group}}', 'AWS', NULL, NULL, 'constuctive', 'descriptive', NULL, '{{Terraform}}', NULL, NULL, NULL);"
+        query = f"INSERT INTO {dn}.incident_configurations (incident_id, incident_type, category, title, guideline, severity, lacework_violation_id, prowler_violation_id, checkov_check_id, remediation_ids, condition_query, is_custom, customer_name, resource_types, provider, created_by, code, constructive_title, descriptive_title, pc_policy_id, frameworks, pc_severity, source_incident_id, additional_pc_policy_ids) VALUES ('{incident_id}', 'Violation', 'General', 'Ensure Security group has correct tags', 'url to guideline', 'MEDIUM', NULL, NULL, '{incident_id}','{{}}', NULL, true, 'johndeere', '{{aws_security_group}}', 'AWS', NULL, NULL, 'constuctive', 'descriptive', NULL, '{{Terraform}}', NULL, NULL, NULL);"
         cur.execute(query)
         query_results = cur.fetchall()
         return query_results
