c7c105dd8d9cf360e1e256142a670a097226fb50
James Woolfenden <james.woolfenden@gmail.com>
2022-07-04 11:47:59 +0100

    WIP

--- aws_s3_object.components.tf
+++ aws_s3_object.components.tf
@@ -1,5 +1,8 @@
 resource "aws_s3_object" "components" {
   bucket = local.full_s3_bucket_name
-  key    = "components"
-  source = "components.list"
+  key    = "components.json"
+  source = "components.json"
+  depends_on = [
+    module.s3
+  ]
 }
